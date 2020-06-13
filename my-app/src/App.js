d5f9c95d67c29ca9527f261412eaa805ee33c835
Sam Gibson <samuelmgibson@gmail.com>
2020-06-13 10:10:25 -0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,66 @@
 import React, { Component } from 'react';
+import {v4 as uuid} from 'uuid';
+import Header from './components/layout/header';
 import Todos from'./components/Todos';
+import AddTodo from './components/AddTodo';
+
+
 
 class App extends Component {
   state ={
     todos: [
       {
-        id: 1,
+        id: uuid(),
         title: 'Drive to SF',
         completed: false
       },
       {
-        id: 2,
+        id: uuid(),
         title: 'Dinner with Nico',
         completed: false
       },
       {
-        id: 3,
+        id: uuid(),
         title: 'Eat Ice Cream',
         completed: false
       }
     ]
+  }
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+  }
+
+  //add Todo
+  AddTodo = (title) =>{
+    const newTodo = {
+      id:uuid(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
   }
 
   render() {
     console.log()
     return (
       <div className="App">
-        <h1>app</h1>
-        <Todos todos={this.state.todos} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.AddTodo}/>
+          <Todos todos={this.state.todos} markComplete={this.markComplete} 
+          delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
--- my-app/src/components/Todos.js
+++ my-app/src/components/Todos.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import TodoItem from './TodoItem';
-
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
-  render() {
+    render() {
     return this.props.todos.map((todo) =>(
-        <TodoItem key={todo.id} todo={todo}/>
+        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo}/>
     ));
-  }
+    }
+}
+//proptypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
 }
 
 export default Todos;
+ 
